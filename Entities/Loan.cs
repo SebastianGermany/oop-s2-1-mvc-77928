acc85a32bb107b7e405440317373ce0fa4d4241d
SebastianL <muse_sebas@hotmail.com>
2026-03-19 15:39:41 +0000

    Add files via upload

---
+++ Entities/Loan.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Domain.Entities
+{
+    public class Loan
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int BookId { get; set; }
+
+        [Required]
+        public int MemberId { get; set; }
+
+        public DateTime LoanDate { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public DateTime? ReturnedDate { get; set; }
+
+        public Book? Book { get; set; }
+
+        public Member? Member { get; set; }
+    }
+}
